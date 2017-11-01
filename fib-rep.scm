60ba9f8f929a1e2af1500f069cd9c0352525fdc4
Chris Brooks <flavenstein@gmail.com>
2017-11-01 01:17:14 -0600

    Added some notes and examples

--- fib-rep.scm
+++ fib-rep.scm
@@ -1,38 +1,41 @@
-; Find the successive Fibonacci numbers f_i, f_i+1 for which f_i < n+1 < f_i+1.
-; Useful for finding the maximal fibonacci representation, see 'fib-base'.
-(define (fibonacci-bounds n)
-  (let loop ([fib 1] [prev 1] [pprev 0])
-    (if (< (+ n 1) fib)
-        (values prev pprev)
-        (loop (+ fib prev) fib prev))))
-
 ; Every positive integer can be written uniquely as a sum of distinct positive
 ; Fibonacci numbers such that no two successive Fibonacci numbers are excluded.
-; This function finds such a representation for any integer, returned as a list
-; of 1's and 0's where the ith entry means that Fibonacci number is included
-; in the sum (starting with 1)
+; In other words, a positive integer can be written as a list of 1's and 0's
+; where the ith entry means that Fibonacci number is included; this list is
+; unique if we require that the list does not contain two zeros in succession.
+; We refer to this representation as the maximal Zeckendorf representation.
 ;
 ; Examples:
-;   * (fib-base 6) -> '(1 1 1) since 6 = 1 + 2 + 3. Does not return '(1 0 0 1)
+;   * (fib-rep 6) -> '(1 1 1) since 6 = 1 + 2 + 3. Does not return '(1 0 0 1)
 ;     since even though 6 = 1 + 5, this excludes two successive fibonaccis.
-;   * (fib-base 18) -> '(0 1 1 1 1) since 18 = 2 + 3 + 5 + 8.
+;   * (fib-rep 18) -> '(0 1 1 1 1) since 18 = 2 + 3 + 5 + 8.
 ;     While 18 can be represented as '(0 0 0 1 0 1) or '(0 1 1 0 0 1), these
 ;     representations include successive zeros
-(define (fib-base n)
-  (let-values ([(fib prev) (fibonacci-bounds n)])
+
+; Finds two values: The highest Fibonacci number present in the maximal
+; Zeckendorf representation of 'n', and the next Fibonacci number.
+(define (fib-bounds n)
+  (let loop ([fib 1] [prev 1] [pprev 0])
+    (if (< (+ n 1) fib)
+        (values pprev prev)
+        (loop (+ fib prev) fib prev))))
+
+; Find the maximal Zeckendorf representation of 'n'
+(define (fib-rep n)
+  (let-values ([(prev fib) (fib-bounds n)])
     (let loop ([n n] [fib fib] [prev prev] [acc '()])
       (cond [(= fib 1) acc]
             [(< (+ n 1) fib) (loop n prev (- fib prev) (cons 0 acc))]
             [else (loop (- n prev) prev (- fib prev) (cons 1 acc))]))))
 
 ; Given a list of integers a_i, 0 <= i <= n, find \sum_{i=0}^n a_i*F_{i+2}
-(define (un-fib-base rep)
+(define (un-fib-rep rep)
   (let loop ([rep rep] [fib 1] [prev 1] [acc 0])
     (if (null? rep) acc
         (loop (cdr rep) (+ fib prev) fib (+ acc (* (car rep) fib))))))
 
 ; All fibonacci representations where no digit is bigger than 'max'
-(define (fib-base* n max)
+(define (fib-rep* n max)
   (let loop ([n n] [fib 1] [prev 1] [count 0])
     (cond [(> count max) '()]
           [(= n 0) `((,count))]
@@ -57,26 +60,28 @@
                   (map (lambda (x) (cons 2 x)) accum)))))))
 
 (define (normalize rep)
-  (fib-base (un-fib-base rep)))
+  (fib-rep (un-fib-rep rep)))
 
 (define (list-addition a b)
   (cond [(null? a) b]
         [(null? b) a]
         [else (cons (+ (car a) (car b)) (list-addition (cdr a) (cdr b)))]))
 
-(define (addition-test n)
+(define (all-sums n)
   (let loop ([sum 2] [a 1] [b 1])
     (cond [(> sum n) '()]
           [(> a b) (loop (+ sum 1) 1 sum)]
-          [else (cons (cons
-                        (list-addition (fib-base a)
-                                       (fib-base b))
-                        (fib-base sum))
+          [else (cons (list-addition (fib-rep a) (fib-rep b))
                       (loop sum (+ a 1) (- b 1)))])))
 
 (define (twos-test n)
   (let loop ([i 0] [li '(2)])
-    (let ([val (un-fib-base li)])
+    (let ([val (un-fib-rep li)])
       (if (> i n) '()
-          (cons (cons li (fib-base val))
+          (cons (cons li (fib-rep val))
                 (loop (+ i 1) (cons 2 li)))))))
+
+ (define (truncate n)
+   (let loop ([li (fib-rep n)])
+     (if (null? li) '()
+         (cons (un-fib-rep li) (loop (cdr li))))))
