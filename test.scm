96e535ea831bb4610615cacfab4999e2e2d27ee8
Chris Brooks <flavenstein@gmail.com>
2017-11-04 14:34:51 -0600

    Test for valid greedy Fibonacci representations

--- test.scm
+++ test.scm
@@ -1,19 +1,25 @@
 (load "addition.scm")
 (load "fib-rep.scm")
 
-(define (test-rep x)
-  (if (equal? (reduce x) (normalize x))
+(define (test-equal li)
+  (if (equal? (reduce li) (normalize li))
       #t
       (begin
-        (printf "~a -> ~a, should be ~a\n" x (reduce x) (normalize x))
+        ;(printf "~a -> ~a, should be ~a\n" li (reduce li) (normalize li))
         #f)))
+
+(define (test-valid l)
+  (let loop ([li (reduce l)] [prev 0])
+    (cond [(null? li) #t]
+          [(< (+ prev (car li)) 2) (loop (cdr li) (car li))]
+          [else #f])))
 
 (define (test-up-to n)
   (let loop ([xs (all-sums n)] [done '()] [fails 0] [passes 0])
     (cond [(null? xs) (values fails passes)]
           [(member (car xs) done)
            (loop (cdr xs) done fails passes)]
-          [(test-rep (car xs))
+          [(test-valid (car xs))
            (loop (cdr xs) (cons (car xs) done) fails (+ passes 1))]
           [else
            (loop (cdr xs) (cons (car xs) done) (+ fails 1) passes)])))
