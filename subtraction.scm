b1c01e43d216f42873718a84ab3b9dd6dbf28ac0
Chris Brooks <flavenstein@gmail.com>
2017-11-26 23:52:26 -0700

    Since each window has at most one reduction, we know that when 'rest' is null, we can start going backwards

--- subtraction.scm
+++ subtraction.scm
@@ -1,15 +1,19 @@
-; Fix the extra zeros at the very end
-(define (b-win a b c d rest acc start?)
-  (cond [(= b 2) (b-win (+ a 1) 0 c (+ d 1) rest acc start?)]
-        [(and (> b 0) (> c 0)) (b-win (+ a 1) (- b 1) (- c 1) d rest acc #f)]
+(define (b-mov a b c d rest acc start?)
+  (cond [(and (null? rest) (null? acc))
+         (append (if (= 0 (+ a b c)) '() (list (+ b c)))
+                 (if (= 0 a) '() (list a)))]
         [(null? rest) (append (list (+ b c) a) acc)]
         [(and start? (= a 0)) (b-win b c d (car rest) (cdr rest) acc #t)]
         [else (b-win b c d (car rest) (cdr rest) (cons a acc) #f)]))
 
+(define (b-win a b c d rest acc start?)
+  (cond [(= b 2) (b-mov (+ a 1) 0 c (+ d 1) rest acc #f)]
+        [(and (> b 0) (> c 0)) (b-mov (+ a 1) (- b 1) (- c 1) d rest acc #f)]
+        [else (b-mov a b c d rest acc start?)]))
+
 (define (f-mov a b c d e rest acc)
-  (cond [(and (null? rest) (= c 0) (= d 0) (= e 0)) (b-win 0 0 b a acc '() #t)]
-        [(null? rest) (f-win b c d e 0 '() (cons a acc))]
-        [else (f-win b c d e (car rest) (cdr rest) (cons a acc))]))
+  (if (null? rest) (b-win e d c b (cons a acc) '() #t)
+      (f-win b c d e (car rest) (cdr rest) (cons a acc))))
 
 (define (f-win a b c d e rest acc)
   (cond [(= c -1) (f-mov a b 0 0 (- e 1) rest acc)]
