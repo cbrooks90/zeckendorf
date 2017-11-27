7d6f309c5324c2cd89f43a794bcee55188a542b6
Chris Brooks <flavenstein@gmail.com>
2017-11-26 21:15:24 -0700

    There were some reduction rules that were never used. Seems suspicious

--- subtraction.scm
+++ subtraction.scm
@@ -11,13 +11,12 @@
         [(null? rest) (f-win b c d e 0 '() (cons a acc))]
         [else (f-win b c d e (car rest) (cdr rest) (cons a acc))]))
 
-; Find the arg combinations such that f-win calls itself and eliminate (if this
-; doesn't result in a huge number of cases)
 (define (f-win a b c d e rest acc)
-  (cond [(and (> b 0) (> c 0)) (f-mov a (- b 1) (- c 1) (+ d 1) e rest acc)]
-        [(= c 2) (f-win (+ a 1) b 0 (+ d 1) e rest acc)]
-        [(= c -1) (f-mov a b 0 0 (- e 1) rest acc)]
-        [(and (= d -1) (> e -1)) (f-win a b (+ c 1) 0 (- e 1) rest acc)]
+  (cond [(= c -1) (f-mov a b 0 0 (- e 1) rest acc)]
+        [(and (= d -1) (> e -1))
+         (cond [(= b 1) (f-mov    a    0 0 1 (- e 1) rest acc)]
+               [(= c 1) (f-mov (+ a 1) 0 0 1 (- e 1) rest acc)]
+               [else    (f-mov    a    0 1 0 (- e 1) rest acc)])]
         [else (f-mov a b c d e rest acc)]))
 
 (define (start a b c d e rest)
