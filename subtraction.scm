ff48c78622a66c77c448e23e7e1f7fa66d31c4ed
Chris Brooks <flavenstein@gmail.com>
2017-11-26 20:21:50 -0700

    Here is a version which is correct and favors reduction with -1 in the middle. It's unclear if this version is better, but this seems to get me closer to making it possible to have at most one reduction per window slide.

--- subtraction.scm
+++ subtraction.scm
@@ -1,3 +1,4 @@
+; Fix the extra zeros at the very end
 (define (b-win a b c d rest acc start?)
   (cond [(= b 2) (b-win (+ a 1) 0 c (+ d 1) rest acc start?)]
         [(and (> b 0) (> c 0)) (b-win (+ a 1) (- b 1) (- c 1) d rest acc #f)]
@@ -11,17 +12,17 @@
         [else (f-win b c d e (car rest) (cdr rest) (cons a acc))]))
 
 ; Find the arg combinations such that f-win calls itself and eliminate (if this
-; doesn't have result in a huge number of cases)
+; doesn't result in a huge number of cases)
 (define (f-win a b c d e rest acc)
-  (cond [(and (= d -1) (= e 1)) (f-win a b (+ c 1) 0 0 rest acc)]
-        [(= c -1) (f-mov a b 0 (+ d 1) (- e 1) rest acc)]
-        [(and (> a 0) (> b 0)) (f-mov (- a 1) (- b 1) (+ c 1) d e rest acc)]
+  (cond [(and (> a 0) (> b 0)) (f-win (- a 1) (- b 1) (+ c 1) d e rest acc)]
         [(= c 2) (f-win (+ a 1) b 0 (+ d 1) e rest acc)]
+        [(= c -1) (f-mov a b 0 (+ d 1) (- e 1) rest acc)]
+        [(and (= d -1) (> e -1)) (f-win a b (+ c 1) 0 (- e 1) rest acc)]
         [else (f-mov a b c d e rest acc)]))
 
 (define (start a b c d e rest)
-  (if (and (= c -1) (= d 1))
-      (f-mov 0 0 1 0 e rest '())
+  (if (= c -1)
+      (f-win 0 0 1 (- d 1) e rest '())
       (f-win a b c d e rest '())))
 
 (define (reduce li)
