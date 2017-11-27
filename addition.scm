54829f8d6c22753e2bf3784f8870299435a10f9c
Chris Brooks <flavenstein@gmail.com>
2017-11-26 22:49:48 -0700

    Function renames to be consistent with subtract

--- addition.scm
+++ addition.scm
@@ -4,30 +4,30 @@
         [(= d 2) (append (list 0 1 0 a) rest)]
         [else (append (list d c b a) rest)]))
 
-(define (wodniw a b c d rest acc)
-  (cond [(and (> c 0) (> d 0)) (wodniw a (+ b 1) (- c 1) (- d 1) rest acc)]
-        [(and (> b 1) (= a 0)) (wodniw (+ a 1) (- b 2) c (+ d 1) rest acc)]
+(define (b-win a b c d rest acc)
+  (cond [(and (> c 0) (> d 0)) (b-win a (+ b 1) (- c 1) (- d 1) rest acc)]
+        [(and (> b 1) (= a 0)) (b-win (+ a 1) (- b 2) c (+ d 1) rest acc)]
         [(null? rest) (end a b c d acc)]
-        [else (wodniw b c d (car rest) (cdr rest) (cons a acc))]))
+        [else (b-win b c d (car rest) (cdr rest) (cons a acc))]))
 
-(define (ecuder li)
+(define (b-start li)
   (cond [(or (null? li)
              (null? (cdr li))
              (null? (cddr li))
              (null? (cdddr li))) (reverse li)]
-        [else (wodniw (car li) (cadr li) (caddr li) (cadddr li) (cddddr li) '())]))
+        [else (b-win (car li) (cadr li) (caddr li) (cadddr li) (cddddr li) '())]))
 
-(define (window a b c d rest acc)
-  (cond [(and (> a 0) (> b 0)) (window (- a 1) (- b 1) (+ c 1) d rest acc)]
-        [(and (> c 1) (= d 0)) (window (+ a 1) b (- c 2) (+ d 1) rest acc)]
-        [(and (null? rest) (= 0 (+ a b c d))) (ecuder acc)]
-        [(null? rest) (window b c d 0 '() (cons a acc))]
-        [else (window b c d (car rest) (cdr rest) (cons a acc))]))
+(define (f-win a b c d rest acc)
+  (cond [(and (> a 0) (> b 0)) (f-win (- a 1) (- b 1) (+ c 1) d rest acc)]
+        [(and (> c 1) (= d 0)) (f-win (+ a 1) b (- c 2) (+ d 1) rest acc)]
+        [(and (null? rest) (= 0 (+ a b c d))) (b-start acc)]
+        [(null? rest) (f-win b c d 0 '() (cons a acc))]
+        [else (f-win b c d (car rest) (cdr rest) (cons a acc))]))
 
 (define (start a b c d rest)
-  (cond [(and (= a 2) (= b 0)) (window 0 1 c d rest '())]
-        [(and (= a 0) (= b 2) (= c 0)) (window 1 0 1 d rest '())]
-        [else (window a b c d rest '())]))
+  (cond [(and (= a 2) (= b 0)) (f-win 0 1 c d rest '())]
+        [(and (= a 0) (= b 2) (= c 0)) (f-win 1 0 1 d rest '())]
+        [else (f-win a b c d rest '())]))
 
 (define (reduce li)
   (cond [(null? li) '()]
